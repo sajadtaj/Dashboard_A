83b068024b4f082cc2149cceadb7495b4c58677a
Xirano <sajadtajco@gmail.com>
2021-07-15 13:16:18 +0430

    dashboard with components

--- dash+DIV.py
+++ dash+DIV.py
@@ -1,25 +1,152 @@
 import dash
 import dash_html_components as html
-import dash_html_components as dcc
+import dash_core_components as dcc
 import dash_bootstrap_components as dbc
+import plotly.express as px
+import  pandas as pd
+import plotly.graph_objects as go
+#------------------------------------------------------------------DATA-------
+df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
+df_1 =  pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv",dtype={"fips": str})
+df_2 = px.data.medals_long()
 #-------------------------------------------------------------------------------
 colors = {
     'background': '#111111',
     'text': '#7FDBFF'
 }
 #-------------------------------------------------------------------------------
+#          figure
+#-------------------------------------------------------------------------------
+fig = px.scatter(df, x="gdp per capita", y="life expectancy",
+                 size="population", color="continent", hover_name="country",
+                 log_x=True, size_max=60)
+fig_1 = px.choropleth(locations=["CA", "TX", "NY"], locationmode="USA-states", color=[1,2,3], scope="usa")
+fig_2 = px.bar(df_2, x="nation", y="count", color="medal", title="Long-Form Input")
+
+#-------------------------------------------------------------------------------
 #           component of dashboard Box
 #-------------------------------------------------------------------------------
-component_slider=html.H2('Slider')
-component_1=html.H2('Box Number 1')
-component_2=html.H2('Box Number 2')
-component_3=html.H2('Box Number 3')
-component_4=html.H2('Box Number 4')
-component_5=html.H2('Box Number 5')
-component_6=html.H2('Box Number 6')
-component_7=html.H2('Box Number 7')
-component_8=html.H2('Box Number 8')
-component_9=html.H2('Box Number 9')
+component_slider=dbc.Card(
+    [
+        dbc.CardHeader("Slider"),
+        dbc.CardBody(
+            [
+                html.H4("slider title", className="card-title"),
+                html.P("describe your perfect date", className="card-text"),
+                html.Br(),
+                dbc.Nav(
+                    [
+                    dbc.NavLink("Nav Link 1", href="/"      , active="exact"),
+                    dbc.NavLink("Nav link 2", href="/page-1", active="exact"),
+                    dbc.NavLink("Nav Link 2", href="/page-2", active="exact"),
+                    ],
+                    vertical=True,
+                    pills=True,
+                )
+            ]
+        ),
+        dbc.CardFooter("designed By Sajad Taj"),
+    ],
+    style={"width": "100%",'height':'50rem', 'text-color':'#0E3854'},
+    color="#F9F1F0",
+
+)
+component_1= dcc.Graph(
+        id='life-exp-vs-gdp',
+        figure=fig
+    )
+component_2= dcc.Graph(
+        id='fertaliyt rate',
+        figure=fig_1
+    )
+component_3=dcc.Graph(
+        id='bar chart',
+        figure=fig_2
+    )
+component_4=dbc.Card(
+    dbc.ListGroup(
+        [
+        dbc.ListGroupItem("The primary item", color="primary"),
+        dbc.ListGroupItem("A secondary item", color="secondary"),
+        dbc.ListGroupItem("A successful item", color="success"),
+        dbc.ListGroupItem("A warning item", color="warning"),
+        dbc.ListGroupItem("A dangerous item", color="danger"),
+        dbc.ListGroupItem("An informative item", color="info"),
+        ],
+        flush=True,
+
+    ),
+    style={"width": "100%", 'height':'14rem'},
+)
+#component_5=html.H2('Box 5')
+component_6=dbc.Card(
+    [
+        dbc.CardBody(
+            [
+                html.H4("Card title", className="card-title"),
+                html.P(
+                    "Some quick example text to build on the card title and "
+                    "make up the bulk of the card's content.",
+                    className="card-text",
+                ),
+                dbc.Button("Go somewhere", color="primary"),
+            ]
+        ),
+    ],
+    style={"width": "100%"},
+    color="info"
+)
+component_7=dbc.Card(
+    [
+        dbc.CardBody(
+            [
+                html.H4("Card title", className="card-title"),
+                html.P(
+                    "Some quick example text to build on the card title and "
+                    "make up the bulk of the card's content.",
+                    className="card-text",
+                ),
+                dbc.Button("Go somewhere", color="primary"),
+            ]
+        ),
+    ],
+    style={"width": "100%"},
+    color="danger"
+)
+component_8=dbc.Card(
+    [
+        dbc.CardBody(
+            [
+                html.H4("Card title", className="card-title"),
+                html.P(
+                    "Some quick example text to build on the card title and "
+                    "make up the bulk of the card's content.",
+                    className="card-text",
+                ),
+                dbc.Button("Go somewhere", color="primary"),
+            ]
+        ),
+    ],
+    style={"width": "100%"},
+    color="warning"
+)
+component_9=dbc.Card(
+    [
+        dbc.CardBody(
+            [
+                html.H4("Card title", className="card-title"),
+                html.P(
+                    "Some quick example text to build on the card title and "
+                    "make up the bulk of the card's content.",
+                    className="card-text",
+                ),
+                dbc.Button("Go somewhere", color="primary"),
+            ]
+        ),
+    ],
+    style={"width": "100%"},
+    color="success"
+)
 
 
 #------------------------------------------------------------------------------
@@ -58,9 +185,9 @@ layer_1=html.Div([
             ),
             html.Div(
                 [
-                html.Div([component_3],style={'width':'95%','border-color':'#D2F6FC','border-radius':'10px' ,'margin':'5px','border-style': 'solid','backgroundColor': '#ffffff','height':'13rem','display': 'inline-block', }),
-                html.Div([component_4],style={'width':'95%','border-color':'#D2F6FC','border-radius':'10px' ,'border-style': 'solid','backgroundColor': '#ffffff','height':'22rem','display': 'inline-block', }),
-                html.Div([component_5],style={'width':'95%','border-color':'#D2F6FC','border-radius':'10px' ,'margin':'5px','border-style': 'solid','backgroundColor': '#ffffff','height':'13rem','display': 'inline-block', }),
+                html.Div([component_3],style={'width':'95%','border-color':'#D2F6FC','border-radius':'10px' ,'border-style': 'solid','backgroundColor': '#ffffff','height':'30rem','display': 'inline-block', }),
+                html.Div([component_4],style={'width':'95%','border-color':'#D2F6FC','border-radius':'10px' ,'border-style': 'solid','backgroundColor': '#ffffff','height':'20rem','display': 'inline-block', }),
+                #html.Div([component_5],style={'width':'95%','border-color':'#D2F6FC','border-radius':'10px' ,'margin':'5px','border-style': 'solid','backgroundColor': '#ffffff','height':'0rem','display': 'inline-block', }),
                 ],style={'width':'20%',
                         #'padding':'10px 10px 10px 10px',
                         'backgroundColor': '#D8E3E9',
